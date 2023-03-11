1339dd31cc4aa7624f1cec7d66f57f588fa930d8
blckclov3r <blckclov3r@gmail.com>
2023-03-11 15:35:07 +0800

    Squashed commit of the following:
    commit 8b0f73ecd99e33661fe0e0d86528f5333ebc9083
    Author: blckclov3r <blckclov3r@gmail.com>
    Date:   Sat Mar 11 15:34:12 2023 +0800
    
        implement useContext

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,51 @@
+import AppContext from "@/components/AppContext";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  const [category, setCategory] = useState<string>("");
+  const [difficulty, setDifficulty] = useState<string>("");
+  const [type, setType] = useState<string>("");
+  const difficulties = [
+    {
+      id: 1,
+      name: "Easy",
+    },
+    {
+      id: 2,
+      name: "Medium",
+    },
+    {
+      id: 3,
+      name: "Hard",
+    },
+  ];
+
+  const types = [
+    {
+      id: 1,
+      name: "Multiple",
+    },
+    {
+      id: 2,
+      name: "True /False",
+    },
+  ];
+  return (
+    <AppContext.Provider
+      value={{
+        category,
+        setCategory,
+        difficulty,
+        setDifficulty,
+        type,
+        setType,
+        difficulties,
+        types,
+      }}
+    >
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  );
 }
